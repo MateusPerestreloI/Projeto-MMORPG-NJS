c089e9523938389e4cba461404a7b3b80fc28b74
Mateus I <mateuspi.0398@gmail.com>
2022-04-18 17:30:03 -0300

    Habilidades do Jogador
    Implementação do código para gerar as habilidades do jogador aleatoriamente

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,28 +1,30 @@
 module.exports.cadastro = function(application, req, res){
-    res.render('cadastro', { validacao: {}, dadosForm: {} });
+    res.render('cadastro', { validacao: {}, dadosForm: {} })
 }
 
 module.exports.cadastrar = function(application, req, res){
     
-    var dadosForm = req.body;
+    var dadosForm = req.body
 
-    req.assert('nome', 'Nome não pode ser vazio').notEmpty();
-    req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
-    req.assert('senha', 'Senha não pode ser vazia').notEmpty();
-    req.assert('casa', 'Casa não pode ser vazia').notEmpty();
+    req.assert('nome', 'Nome não pode ser vazio').notEmpty()
+    req.assert('usuario', 'Usuário não pode ser vazio').notEmpty()
+    req.assert('senha', 'Senha não pode ser vazia').notEmpty()
+    req.assert('casa', 'Casa não pode ser vazia').notEmpty()
 
-    var erros = req.validationErrors();
+    var erros = req.validationErrors()
 
     if(erros){
-        res.render('cadastro', { validacao: erros, dadosForm: dadosForm });
-        return;
+        res.render('cadastro', { validacao: erros, dadosForm: dadosForm })
+        return
     }
 
-    var connection = application.config.dbConnection;
-    var UsuarioDAO = new application.app.models.UsuarioDAO(connection);
+    var connection = application.config.dbConnection
+    var UsuarioDAO = new application.app.models.UsuarioDAO(connection)
+    var JogoDAO = new application.app.models.JogoDAO(connection)
 
-    UsuarioDAO.inserirUsuario(dadosForm);
+    UsuarioDAO.inserirUsuario(dadosForm)
+    JogoDAO.gerarParametros(dadosForm.usuario)
 
-    res.send('podemos cadastrar');
+    res.send('podemos cadastrar')
 
 }
