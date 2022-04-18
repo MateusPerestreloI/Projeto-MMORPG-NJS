f538d37346b272ecfcb4248bc7b86768309aaa61
Mateus I <mateuspi.0398@gmail.com>
2022-04-18 18:04:45 -0300

    Restringindo acessos e ação de súdito
    Restringindo acesso a página súditos e pergaminhos. E implementando as ações dos súditos

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,7 +1,12 @@
 module.exports.jogo = function (application, req, res) {
 
     if (req.session.autorizado != true) {
-        res.send('Usuário precisa fazre login')
+        res.send('Usuário precisa fazer login')
+    }
+
+    var comando_invalido = 'N'
+    if(req.query.comando_invalido == 'S'){
+        comando_invalido = 'S'
     }
 
     var usuario = req.session.usuario
@@ -10,7 +15,7 @@ module.exports.jogo = function (application, req, res) {
     var connection = application.config.dbConnection
     var JogoDAO = new application.app.models.JogoDAO(connection)
 
-    parametros = JogoDAO.iniciarjogo(usuario, res, casa)
+    parametros = JogoDAO.iniciarjogo(usuario, res, casa, comando_invalido)
 }
 
 module.exports.sair = function (application, req, res) {
@@ -22,10 +27,40 @@ module.exports.sair = function (application, req, res) {
 
 module.exports.suditos = function (application, req, res) {
 
-        res.render('aldeoes', { validacao: {} })
+    if (req.session.autorizado != true) {
+        res.send('Usuário precisa fazer login')
+    }
+
+    res.render('aldeoes', { validacao: {} })
 }
 
 module.exports.pergaminhos = function (application, req, res) {
 
-        res.render('pergaminhos', { validacao: {} })
+    if (req.session.autorizado != true) {
+        res.send('Usuário precisa fazer login')
+    }
+
+    res.render('pergaminhos', { validacao: {} })
+}
+
+module.exports.ordenar_acao_sudito = function (application, req, res) {
+
+    if (req.session.autorizado != true) {
+        res.send('Usuário precisa fazer login')
+    }
+
+    var dadosForm = req.body
+
+    req.assert('acao', 'Ação deve ser informado').notEmpty()
+    req.assert('quantidade', 'Quantidade deve ser informado').notEmpty()
+
+    var erros = req.validationErrors()
+
+    if(erros)
+    {
+        res.redirect('jogo')
+        return
+    }
+
+    res.send('Tudo ok')
 }
--- app/models/JogoDAO.js
+++ app/models/JogoDAO.js
@@ -20,11 +20,11 @@ JogoDAO.prototype.gerarParametros = function (usuario) {
     })
 }
 
-JogoDAO.prototype.iniciarjogo = function (usuario, res, casa) {
+JogoDAO.prototype.iniciarjogo = function (usuario, res, casa, comando_invalido) {
     this._connection.open(function (err, mongoclient) {
         mongoclient.collection("jogo", function (err, collection) {
             collection.find({ usuario: usuario }).toArray(function (err, result) {
-                res.render('jogo', { img_casa: casa, jogo: result[0] })
+                res.render('jogo', { img_casa: casa, jogo: result[0], comando_invalido: comando_invalido })
 
                 mongoclient.close()
             })
